a58f36e4d16e0fa24f192ed4f3d34eda985e74a7
sujithachalla97 <168841679+sujithachalla97@users.noreply.github.com>
2024-07-12 20:45:21 +0530

    Update about.js

--- about.js
+++ about.js
@@ -204,3 +204,21 @@ document.addEventListener("DOMContentLoaded", function() {
         revealObserver.observe(reveal);
     });
 });
+
+
+/**
+ * header & go-top-btn active
+ * when window scroll down to 400px
+ */
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY >= 400) {
+        header.classList.add("active");
+        goTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        goTopBtn.classList.remove("active");
+    }
+});
